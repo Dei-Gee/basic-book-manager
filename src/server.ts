e5976b3edf92b4a658359f9d1bd41928307eece3
Dei-Gee <deji2109@gmail.com>
2020-08-13 14:32:18 -0700

    added node scripts

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,26 @@ const app:Application = express();
 app.options('*', cors()); // preflight OPTIONS; put before other routes
 
 
-app.use(function (req: Request, res:Response, next:NextFunction) {
+app.use((req: Request, res:Response, next:NextFunction) => {
 
     // Website you wish to allow to connect: In this case, the frontend running on port 3000
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
+
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
+
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
-  
+
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  
+
     // Pass to next layer of middleware
     next();
-  });
+});
+
+app.get('/', (req: Request, res:Response) => {
+    res.send('Hello World!');
+});
 
 //   BodyParser Middleware
 app.use(bodyParser.urlencoded({
@@ -39,4 +43,4 @@ app.use(bodyParser.json());
 // port: The app(server) will listen on this port
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
